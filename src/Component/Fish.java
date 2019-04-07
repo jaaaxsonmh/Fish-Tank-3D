a9019909dc89cb51078317d603d4de880948ba74
Jack hosking <jackhosking2@outlook.com>
2019-04-08 00:21:52 +1200

    key listener, drawable

--- src/Component/Fish.java
+++ src/Component/Fish.java
@@ -4,6 +4,7 @@ package Component;
 import com.jogamp.opengl.GL2;
 import com.jogamp.opengl.glu.GLU;
 import com.jogamp.opengl.glu.GLUquadric;
+import utils.AXIS;
 import utils.Drawable;
 
 import java.util.LinkedList;
@@ -14,10 +15,12 @@ public class Fish implements Drawable {
     private double radius;
     private double height;
     private double rotationAngle, transX, transY, transZ;
+    private AXIS axis;
 
-    Fish(double radius, double height) {
+    Fish(double radius, double height, AXIS axis) {
         this.radius = radius;
         this.height = height;
+        this.axis = axis;
     }
 
     @Override
@@ -27,16 +30,30 @@ public class Fish implements Drawable {
         // TODO TRANSFORM
 
         // draw each child
-        for(Fish child : children) {
+        for (Fish child : children) {
             child.draw(gl, glu, quadric, filled);
         }
 
         gl.glPopMatrix();
-
     }
 
     private void transformNode(GL2 gl) {
         // do the translation relative to the parent and then rotate and move nodes
+        gl.glTranslated(transX, transY, transZ);
+
+        switch (axis) {
+            case X:
+                gl.glRotated(rotationAngle, 1, 0, 0);
+                break;
+            case Y:
+                gl.glRotated(rotationAngle, 0, 1, 0);
+                break;
+            case Z:
+                gl.glRotated(rotationAngle, 0, 0, 1);
+                break;
+            default:
+                break;
+        }
     }
 
     private void setTranslations(double x, double y, double z) {
